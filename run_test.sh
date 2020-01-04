5e1ef744ab65e34a7e503c3bcc10172392bc889a
liuyihao <liuyihao14@mails.ucas.ac.cn>
2020-01-04 17:08:00 +0800

    update dockerfile

--- run_test.sh
+++ run_test.sh
@@ -42,7 +42,7 @@ do
  echo $folder/$filename
  echo $save_folder/${filename%.*}
  
- #mkdir -p $save_folder
+ mkdir -p $save_folder
  
  ffmpeg -r 24000/1001 -i $folder/$filename/%3d.png -vcodec libx265 -pix_fmt yuv422p -crf 10 $save_folder/${filename}.mp4 -y
  
